30939eea2e4d24d3d651fedb0d767528a6434e05
KH515 <owow1440@gmail.com>
2025-09-27 12:01:46 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,5 @@
-// تحميل الهيدر من المجلد بدون امتداد
-fetch('/includes/header/')
+fetch('/includes/header/index.html')
   .then(response => response.text())
   .then(data => {
     document.getElementById('header-placeholder').innerHTML = data;
-  });
-
-// تحميل الفوتر من المجلد بدون امتداد
-fetch('/includes/footer/')
-  .then(response => response.text())
-  .then(data => {
-    document.getElementById('footer-placeholder').innerHTML = data;
   });
