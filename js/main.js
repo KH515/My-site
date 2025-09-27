d87404cbf703571d911a626519f72e2a37a0d2bb
KH515 <owow1440@gmail.com>
2025-09-27 11:08:13 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-
+// تحميل الفوتر في كل صفحة
+fetch('/includes/footer.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('footer-placeholder').innerHTML = data;
+  });
